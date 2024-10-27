9fe8d67abc25c9301dea4e6e5c0a58c6fc13ac54
Mohammad Shabeer <moshdev2213@gmail.com>
2024-10-27 23:15:06 +0530

    feat:codeDeployInit

---
+++ start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull moshdev2213/noob-codebuild-app
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 moshdev2213/noob-codebuild-app
